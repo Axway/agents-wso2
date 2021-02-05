e7eece177623e118a5983fc2b628a8a15576e308
mprojtz <mprojtz@axway.com>
2021-02-05 10:17:22 -0700

    Made the rest port configurable.

--- traceability/pkg/config/config.go
+++ traceability/pkg/config/config.go
@@ -17,6 +17,7 @@ type GatewayConfig struct {
 	corecfg.IConfigValidator
 	LogFile        string `config:"logFile"`
 	ProcessOnInput bool   `config:"processOnInput"`
+	RestPort       string `config:"restPort"`
 	ConfigKey1     string `config:"config_key_1"`
 	ConfigKey2     string `config:"config_key_2"`
 	ConfigKey3     string `config:"config_key_3"`
@@ -26,6 +27,9 @@ type GatewayConfig struct {
 func (c *GatewayConfig) ValidateCfg() (err error) {
 	if c.LogFile == "" {
 		return errors.New("Invalid gateway configuration: logFile is not configured")
+	}
+	if c.RestPort == "" {
+		return errors.New("Invalid gateway configuration: restPort is not configured")
 	}
 
 	return
